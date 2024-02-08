070c3b31c091c485c5192ddc7c9ff4492b6c8ac3
Grace Luka <graceluka@mines.edu>
2024-02-08 14:49:39 -0700

    Continued to work on the moveing tests after we realized out code need to be changed from the canvas examples. Grace is driving

--- C07C-KerPlop/src/tests/TestInteractions.java
+++ C07C-KerPlop/src/tests/TestInteractions.java
@@ -11,6 +11,8 @@ import levelPieces.Frog;
 import levelPieces.Snake;
 import levelPieces.Key;
 import levelPieces.Doorway;
+import levelPieces.Blackhole;
+import levelPieces.Bridge;
 import gameEngine.Drawable;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
@@ -28,7 +30,7 @@ class TestInteractions {
 		for (int i = 0; i < 6; i++){
 			assertEquals(InteractionResult.NONE, jumpy.interact(gameBoard, i));
 		}
-		assertEquals(InteractionResult.NONE, jumpy.interact(gameBoard, 7))
+		assertEquals(InteractionResult.NONE, jumpy.interact(gameBoard, 7));
 		for (int i = 9; i < GameEngine.BOARD_SIZE; i++){
 			assertEquals(InteractionResult.NONE, jumpy.interact(gameBoard, i));
 		}
@@ -63,14 +65,47 @@ class TestInteractions {
 	}
 
 	@Test
+	void testBridge() {
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Doorway oak = new Doorway('-', "Bridge", 9);
+		gameBoard[9] = oak;
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++){
+			assertEquals(InteractionResult.NONE, oak.interact(gameBoard, i));
+		}
+	}
+
+	@Test
 	void testDoorway() {
 		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
-		Doorway revolving = new Doorway('d', "Doorway", 5);
+		Doorway revolving = new Doorway('d', "Doorway", 15);
+		assertEquals(InteractionResult.ADVANCE, revolving.interact(gameBoard, 15));
 		gameBoard[5] = revolving;
-		for (int i = 0; i < GameEngine.BOARD_SIZE; i++){
+		for (int i = 0; i < 15; i++){
+			assertEquals(InteractionResult.NONE, revolving.interact(gameBoard, i));
+		}
+		for (int i = 17; i < GameEngine.BOARD_SIZE; i++){
 			assertEquals(InteractionResult.NONE, revolving.interact(gameBoard, i));
 		}
 	}
+
+	@Test
+	void testBlackhole(){
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Blackhole swirly = new Blackhole('b', "Blackhole", 12);
+		gameBoard[12] = swirly;
+		assertEquals(InteractionResult.KILL, swirly.interact(gameBoard, 11));
+		assertEquals(InteractionResult.KILL, swirly.interact(gameBoard, 12));
+		assertEquals(InteractionResult.KILL, swirly.interact(gameBoard, 13));
+
+		for (int i = 0; i < 11; i++){
+			assertEquals(InteractionResult.NONE, swirly.interact(gameBoard, i));
+		}
+		for (int i = 14; i < GameEngine.BOARD_SIZE; i++){
+			assertEquals(InteractionResult.NONE, swirly.interact(gameBoard, i));
+		}
+	}
+
+	
 
 	
 
