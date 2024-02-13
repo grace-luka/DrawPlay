6d901a497c70bdd771c12913f03991893a2b1b89
Caroline Schreier <schreier.cb@gmail.com>
2024-02-13 16:25:15 -0700

    Shifted everything 10 to the right and 10 down.

--- C11C-DrawPlay/C11C-DrawPlay/src/Cat.java
+++ C11C-DrawPlay/C11C-DrawPlay/src/Cat.java
@@ -7,9 +7,9 @@ public class Cat {
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 60;
 	// eyes will be about 1/6 from top of head and 1/6 from left
-	private static final int EYE_Y = HEAD_DIMENSION/8;
+	private static final int EYE_Y = HEAD_DIMENSION/8 + 10;
 	private static final int EYE_X = HEAD_DIMENSION/8;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/3 + 10;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
@@ -17,33 +17,33 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2 + 10;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3 + 10;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		int x=catX;
-		int y=catY;
+		int x=catX + 10;
+		int y=catY + 10;
 		// Draw the head
 		g2.setColor(Color.orange);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.blue);
-		x = catX + EYE_X; 
-		y = catY + EYE_Y;
+		x = catX + EYE_X + 10; 
+		y = catY + EYE_Y + 10;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
 		g2.setColor(Color.pink);
-		x = catX + MOUTH_X;
-		y = catY + MOUTH_Y;
+		x = catX + MOUTH_X + 10;
+		y = catY + MOUTH_Y + 10;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.magenta);
 		// Woof text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("I'm a tennis ball", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("I'm a tennis ball", catX + 10, catY + HEAD_DIMENSION +10);	
 	}
 }
