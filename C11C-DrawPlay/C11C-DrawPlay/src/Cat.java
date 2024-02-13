c4bb5d16f0bed509ac106ff207f2ff846c7bf757
Grace Luka <graceluka@mines.edu>
2024-02-13 16:28:45 -0700

    Changed the location of the the tennis ball string and added a sound effect string.

--- C11C-DrawPlay/C11C-DrawPlay/src/Cat.java
+++ C11C-DrawPlay/C11C-DrawPlay/src/Cat.java
@@ -44,6 +44,7 @@ public class Cat {
 		g2.setColor(Color.magenta);
 		// Woof text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("I'm a tennis ball", catX + 10, catY + HEAD_DIMENSION +10);	
+		g2.drawString("I'm a tennis ball", catX + 50, catY + HEAD_DIMENSION +50);
+		g2.drawString("boing...boing...boing",catX + 100, catY + 10);
 	}
 }
